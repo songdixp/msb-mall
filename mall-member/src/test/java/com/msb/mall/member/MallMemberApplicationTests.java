576f32d481377203d49c9a94ce6284c61ac4b03c
sason <sason_git@126.com>
2023-03-30 15:49:00 +0800

    第一次提交：三高系统，项目初始化

--- mall-member/src/test/java/com/msb/mall/member/MallMemberApplicationTests.java
+++ mall-member/src/test/java/com/msb/mall/member/MallMemberApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MallMemberApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
