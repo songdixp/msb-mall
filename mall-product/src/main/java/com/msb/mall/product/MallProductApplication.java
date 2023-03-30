fb62baeef14fc4086aa08209331e410429941eef
sason <sason_git@126.com>
2023-03-30 15:51:27 +0800

    第一次提交：三高系统，项目初始化

--- mall-product/src/main/java/com/msb/mall/product/MallProductApplication.java
+++ mall-product/src/main/java/com/msb/mall/product/MallProductApplication.java
@@ -1,13 +1,13 @@
-package com.msb.mall.ware;
+package com.msb.mall.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MallWareApplication {
+public class MallProductApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MallWareApplication.class, args);
+        SpringApplication.run(MallProductApplication.class, args);
     }
 
 }
